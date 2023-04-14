71a5d2856d4f79f9ced63c4d202b9e4a34a92ee4
Ethan <61816315+RubyyX@users.noreply.github.com>
2023-04-14 22:39:21 +0930

    Update rocket_leauge_offers.js
    comments

--- rocket_leauge_offers.js
+++ rocket_leauge_offers.js
@@ -1,3 +1,6 @@
+// Program that will go through the rocket leage market place online and determine all of the offers and store them in an array
+// This made it easier to see the distribution of bids and offers, making it easier to navigate the market
+
 var posts = document.getElementsByTagName("tbody")[0].getElementsByTagName("tr").length - 1;
 var postsArray = [];
 for(let i=1; i<posts; i++){

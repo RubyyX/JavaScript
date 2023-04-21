2d455ff541a286c669f3947db818938f3353b986
Ethan <61816315+RubyyX@users.noreply.github.com>
2023-04-21 12:03:50 +0930

    Create automatic-10fastfingers.js
    Created file

---
+++ automatic-10fastfingers.js
@@ -0,0 +1,54 @@
+// Program will read words from 10fastfingers.com and input them word by word, waiting for the user to input the word using the spacebar :P
+// Yes, this can be used to cheat. Please use this script responsibly!
+
+// Let's pretend to type at 150 WPM!
+(async () => {
+  const targetWPM = 150;
+  // Calculate delay between letters
+  const delay = (60 / targetWPM) * 1000 / 5;
+
+  // Wait for user to press spacebar
+  async function waitForSpacebar() {
+    return new Promise((resolve) => {
+      function handleSpacebar(event) {
+        if (event.code === 'Space') {
+          document.removeEventListener('keyup', handleSpacebar);
+          resolve();
+        }
+      }
+      document.addEventListener('keyup', handleSpacebar);
+    });
+  }
+
+  // Type a single letter with delay
+  async function typeLetter(letter) {
+    const inputElement = document.querySelector('#inputfield');
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        inputElement.value += letter;
+        inputElement.dispatchEvent(new InputEvent('input'));
+        resolve();
+      }, delay);
+    });
+  }
+
+  // Type an entire word
+  async function typeWord(word) {
+    const letters = word.textContent.split('');
+    for (const letter of letters) {
+      await typeLetter(letter);
+    }
+  }
+
+  // Main typing test loop
+  async function runTypingTest() {
+    const words = document.querySelectorAll('#words span');
+    for (let i = 0; i < words.length - 1; i++) {
+      await typeWord(words[i]);
+      await waitForSpacebar();
+    }
+    await typeWord(words[words.length - 1]);
+  }
+
+  runTypingTest();
+})();
